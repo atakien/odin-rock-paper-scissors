0cbc2994ad3a52f415c78d6790e3a9b4b52974bd
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-08-23 12:53:36 +0300

    Random choice of the computer is developed
    Using math random and floor functions, a random
    number between 0 and 3 is generated and matched
    with options array elements based on conditions.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+const randomNumber = Math.floor(Math.random()*3);
+const options =["Rock", "Paper", "Scissors"]
+    console.log(randomNumber);
+const getComputerChoice = function (){
+   let computerChoice;
+    if(randomNumber === 0){
+computerChoice = options[0]
+    }
+    if(randomNumber === 1){
+computerChoice = options[1]
+    }
+    if(randomNumber === 2){
+computerChoice = options[2]
+    }
+    return computerChoice;
+}
+console.log(getComputerChoice());
